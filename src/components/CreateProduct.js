bc5a31314ac421b5c6631ba7175cd089e6203558
sh <sandrahammamitlili@gmail.com>
2023-07-01 15:10:23 +0200

    interfaces add et edit

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import {Box,Button,TextField , MenuItem ,FormControl} from '@mui/material';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
 import axios from "axios";
 
 const CreateProduct = ({ addProduct, scategories }) => {
@@ -12,10 +16,14 @@ const CreateProduct = ({ addProduct, scategories }) => {
   const [imageart, setImageart] = useState("");
   const [scategorieID, setScategorieID] = useState("");
 
+  const [validated, setValidated] = useState(false);
+
   const URL = "http://localhost:3001/api/"
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const form = e.currentTarget;
+   if (form.checkValidity() === true) {
     const newProduct = {
       reference,
       designation,
@@ -41,99 +49,137 @@ console.log(response)
     setQtestock('');
     setImageart('');
     setScategorieID('');
-  })    
-  
+    setValidated(false);
+  })   
+  .catch(error=>{
+    console.log(error)
+    alert("Erreur ! Insertion non effectuée")
+    })
+  }
+  setValidated(true);     
 }
 
+const handleReset=()=>{
+  setReference('');
+    setDesignation('');
+    setPrix('');
+    setMarque('');
+    setQtestock('');
+    setImageart('');
+    setScategorieID('');
+    setValidated(false);
+}
 
   return (
     <div>
       <h2>Create Product</h2>
-      <center>
-      <Box style={{width:"50%"}}>
-      <FormControl  > 
-            <TextField
-                          variant="outlined"
-                          label="Désignation"
-                          value={designation}
-                          onChange={e => setDesignation(e.target.value)}
-                          required />
-             </FormControl>
-                
-             <FormControl  > 
-             <TextField
-                        variant="outlined"
-                        label="Référence"
-                        value={reference}
-                        onChange={e => setReference(e.target.value)}
-                        required />
-             </FormControl> 
-                        
-             <FormControl  >  
-             <TextField
-                        variant="outlined"
-                        label="Prix"
-                        type="number"
-                        value={prix}
-                        onChange={e => setPrix(e.target.value)}
-                  /> 
-             </FormControl> 
-   
-              <FormControl  >  
-             <TextField
-                        variant="outlined"
-                          label="Image"
-                          type="text"
-                          value={imageart}
-                          onChange={e => setImageart(e.target.value)}
-                  /> 
-             </FormControl>   
-           
-           <FormControl  >          
-           <TextField
-                      variant="outlined"
-                          label="Quantité Stock"
-                          type="number"
-                          value={qtestock}
-                          onChange={e => setQtestock(e.target.value)}
-                  /> 
-           </FormControl>  
-           
-           <FormControl  >      
-           <TextField
-                    fullWidth
-                      style={{ marginLeft: 8}}
-                      variant="outlined"
-                      label="Marque"
-                      value={marque}
-                      onChange={e => setMarque(e.target.value)}
-            /> 
-         </FormControl>  
-                   
-          <FormControl style={{width:350}}>
-                   <TextField
-                   select
-                   label="S/Catégorie"
-                   style={{ marginLeft: 8}}
-                   value={scategorieID}
-                   variant="outlined"
-                   helperText="Sélectionner une s/catégorie"
-                 onChange={e => setScategorieID(e.target.value)}
-                    >
-               {
-               scategories ?    
-               scategories.map((scat)=>
-                    <MenuItem  key={scat._id} value={scat._id}>{scat.nomscategorie}</MenuItem>
-               )
-               :null
-               }
-             </TextField>
-          </FormControl>
-          <div>
-         <Button variant="contained" color="success" onClick={(event)=>handleSubmit(event)}>Ajout</Button>
-          </div>
-          </Box>
-          </center>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+ 
+  <div className="container w-100 d-flex justify-content-center">
+  <div>
+  
+  <div className='form mt-3'>
+  <Row className="mb-2">
+  <Form.Group as={Col} md="6" >
+  <Form.Label >Référence *</Form.Label>
+  <Form.Control
+  required
+  type="text"
+  placeholder="Référence"
+  value={reference}
+  onChange={(e)=>setReference(e.target.value)}
+  />
+  <Form.Control.Feedback type="invalid">
+  Saisir Référence Article
+  </Form.Control.Feedback>
+  </Form.Group>
+  <Form.Group as={Col} md="6">
+  <Form.Label>Désignation *</Form.Label>
+  <Form.Control
+  required
+  type="text"
+  placeholder="Désignation"
+  value={designation}
+  onChange={(e)=>setDesignation(e.target.value)}
+  />
+  <Form.Control.Feedback type="invalid">
+  Saisir Désignation
+  </Form.Control.Feedback>
+  </Form.Group>
+  </Row>
+  <Row className="mb-2">
+  <Form.Group className="col-md-6">
+  <Form.Label>Marque *</Form.Label>
+  <InputGroup hasValidation>
+  <Form.Control
+  type="text"
+  required
+  placeholder="Marque"
+  value={marque}
+  onChange={(e)=>setMarque(e.target.value)}
+  />
+  <Form.Control.Feedback type="invalid">
+  Marque Incorrecte
+  </Form.Control.Feedback>
+  </InputGroup>
+  </Form.Group>
+  <Form.Group as={Col} md="6">
+<Form.Label>Prix</Form.Label>
+<Form.Control
+type="number"
+placeholder="Prix"
+value={prix}
+onChange={(e)=>setPrix(e.target.value)}
+/>
+</Form.Group>
+</Row>
+<Row className="mb-3">
+<Form.Group className="col-md-6 ">
+<Form.Label>
+Qté stock<span className="req-tag">*</span>
+</Form.Label>
+<Form.Control
+required
+type="number"
+value={qtestock}
+onChange={(e)=>setQtestock(e.target.value)}
+placeholder="Qté stock"
+/>
+<Form.Control.Feedback type="invalid">
+Qté stock Incorrect
+</Form.Control.Feedback>
+</Form.Group>
+<Form.Group as={Col} md="6">
+<Form.Label>Image</Form.Label>
+<Form.Control
+type="text"
+placeholder="Image"
+value={imageart}
+onChange={(e)=>setImageart(e.target.value)}
+/>
+</Form.Group>
+<Form.Group as={Col} md="12">
+<Form.Label>S/Catégorie</Form.Label>
+<Form.Control
+as="select"
+type="select"
+value={scategorieID}
+onChange={(e)=>setScategorieID(e.target.value)}
+>
+<option></option>
+{scategories.map((scat)=><option key={scat._id}
+value={scat._id}>{scat.nomscategorie}</option>
+)}
+</Form.Control>
+</Form.Group>
+</Row>
+</div>
+</div>
+</div>
+
+<Button type="submit">Enregistrer</Button>
+<Button type="button" className="btn btn-warning" onClick={()=>handleReset()}>Annuler</Button>
+</Form>
     </div>
   );
 };
